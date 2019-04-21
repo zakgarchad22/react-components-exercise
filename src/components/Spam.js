247663114d18e3316a074cb71ba83e4c1b4b66e4
Tal Bronfer <bronfer@gmail.com>
2019-04-21 14:20:50 +0300

    [ci skip]

--- src/components/Spam.js
+++ src/components/Spam.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 
+class Spam extends Component {
+
+    render() {
+
+        }
+        return 
+    
+}
 
 export default Spam
